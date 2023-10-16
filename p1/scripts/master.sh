40dfb456e929afe700f0581009a46fc8acab530c
Benjamin Brassart <benjaminbrassart@outlook.com>
2023-10-16 19:13:22 +0200

    Make master and worker connect together. Yay!

--- p1/scripts/master.sh
+++ p1/scripts/master.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env sh
 
+set -x
+
+master_address="192.168.56.110"
+
+export INSTALL_K3S_EXEC="server \
+--cluster-init \
+--bind-address='${master_address}' \
+--node-ip='${master_address}' \
+"
+
 curl -sfL 'https://get.k3s.io' | sh -
-cp /etc/rancher/k3s/k3s.yaml /vagrant/kubeconfig
-cp /var/lib/rancher/k3s/server/token /vagrant/k3s_token
+
+# yq -yi '.clusters[].cluster.server = "https://bbrassarS:6443"' /etc/rancher/k3s/k3s.yaml
+
+# systemctl restart k3s
+
+cp /etc/rancher/k3s/k3s.yaml /vagrant_shared/kubeconfig
+cp /var/lib/rancher/k3s/server/token /vagrant_shared/k3s_token
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env sh
 
-curl -sfL 'https://get.k3s.io' | K3S_URL='https://bbrassarS:6443' K3S_TOKEN_FILE='/vagrant/k3s_token' sh -
+set -x
+
+master_address="192.168.56.110"
+node_address="192.168.56.111"
+
+echo "${master_address}  bbrassarS" >> /etc/hosts
+
+token="$(cat /vagrant_shared/k3s_token)"
+
+export KUBECONFIG=/vagrant_shared/kubeconfig
+export INSTALL_K3S_EXEC="agent \
+--node-ip='${node_address}' \
+--server='https://bbrassarS:6443' \
+--token='${token}' \
+"
+
+curl -sfL 'https://get.k3s.io' | K3S_KUBECONFIG_MODE="644" sh -
