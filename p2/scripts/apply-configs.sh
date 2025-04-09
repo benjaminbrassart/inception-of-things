3a49abf8f1c3bfafd657d5fe1e6192907d3dad36
Benjamin Brassart <bbrassar@student.42.fr>
2025-04-09 16:58:05 +0200

    Remove shell loop for applying configs

--- p2/scripts/apply-configs.sh
+++ p2/scripts/apply-configs.sh
@@ -2,8 +2,7 @@
 
 set -e
 
-config_files='app1.yaml app2.yaml app3.yaml ingress.yaml'
-
-for file in ${config_files}; do
-    kubectl apply -f "${file}"
-done
+kubectl apply -f conf/app1.yaml
+kubectl apply -f conf/app2.yaml
+kubectl apply -f conf/app3.yaml
+kubectl apply -f conf/ingress.yaml
