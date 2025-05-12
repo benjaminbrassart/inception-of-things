4020c51f53c9fbfd0c751e9f84cb2421e9e7e715
Benjamin Brassart <bbrassar@student.42.fr>
2025-05-12 20:25:36 +0200

    Setup (and cleanup) argocd in its own namespace

--- p3/scripts/setup.sh
+++ p3/scripts/setup.sh
@@ -1,1 +1,17 @@
 #!/usr/bin/env sh
+
+set -e
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+argocd login --core --insecure
+
+# https://stackoverflow.com/a/68495551
+initial_password="$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
+
+kubectl -n argocd delete secret argocd-initial-admin-secret
+
+kubectl port-forward svc/argocd-server -n argocd --address 0.0.0.0 8080:443
+
+printf -- 'ArgoCD initial password: %s\n' "${initial_password}"
