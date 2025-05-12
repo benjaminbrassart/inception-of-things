585bc159be367be533bb12901d25e048382ed413
Benjamin Brassart <bbrassar@student.42.fr>
2025-05-12 14:10:34 +0200

    Add deinit script

---
+++ p3/scripts/deinit.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+set -e
+
+docker system prune -f
+apt-get remove -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+rm -f \
+    /etc/apt/keyrings/docker.asc \
+    /etc/apt/sources.list.d/docker.list \
+    /etc/profile.d/kubectl.sh
