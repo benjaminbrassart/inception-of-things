8fc801eef88126e5443a61c4c7a765034b76540b
Benjamin Brassart <bbrassar@student.42.fr>
2025-05-12 12:30:01 +0200

    Install docker in init script

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -1,6 +1,33 @@
 #!/usr/bin/env sh
 
-# TODO install docker
+set -e
+
+INIT_USER="$(whoami)"
+export INIT_USER
+
+# Install docker
+sudo sh -c <<-'EOF'
+set -e
+
+apt-get -y update
+apt-get install -y ca-certificates curl
+
+install -m 0755 -d /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+chmod a+r /etc/apt/keyrings/docker.asc
+
+os_arch="$(dpkg --print-architecture)"
+os_release="$(. /etc/os-release && echo "$VERSION_CODENAME")"
+
+printf 'deb [arch=%s signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian %s stable' -- "${os_arch}" "${os_release}" | \
+    tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt-get -y update
+apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+usermod -aG docker "${INIT_USER}"
+EOF
+
 # TODO install k3s
 # TODO install k3d
 # TODO add k=kubectl alias
