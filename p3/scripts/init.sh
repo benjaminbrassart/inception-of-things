526667116902f1a837830b2ddbfd92bfd0a56451
Benjamin Brassart <bbrassar@student.42.fr>
2025-05-12 13:26:57 +0200

    Add k3s install script

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -2,6 +2,14 @@
 
 set -e
 
+tmp="$(mktemp -d)"
+
+trap 'rm -rf "${tmp}"' EXIT
+
+k3s_install_url="https://raw.githubusercontent.com/k3s-io/k3s/079ffa8d99fb859cb8c001455e47efa65535d832/install.sh"
+k3s_install_sha256="d75e014f2d2ab5d30a318efa5c326f3b0b7596f194afcff90fa7a7a91166d5f7"
+k3s_install="${tmp}/k3_install.sh"
+
 # Install docker
 apt-get -y update
 apt-get install -y ca-certificates curl
@@ -21,7 +29,23 @@ apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin do
 
 docker run hello-world > /dev/null
 
-# TODO install k3s
+node_ip4="$(hostname -i)"
+
+# Download k3s install script
+curl -fsSL "${k3s_install_url}" -o "${k3s_install}"
+
+# Verify install scripts checksums
+{
+    printf -- '%s  %s\n' "${k3s_install_sha256}" "${k3s_install}"
+} | sha256sum -c -
+
+# Install k3s
+sh "${k3s_install}" \
+    --cluster-init \
+    --write-kubeconfig-mode=644 \
+    --node-ip="${node_ip4}" \
+    --bind-address="${node_ip4}"
+
 # TODO install k3d
 
 printf -- 'alias k=kubectl\n' > /etc/profile.d/kubectl.sh
