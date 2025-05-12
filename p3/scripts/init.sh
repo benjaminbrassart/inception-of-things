50d87e9f07cfbf0c0abc6fb8c6dbdabfc5f75bcd
Benjamin Brassart <bbrassar@student.42.fr>
2025-05-12 12:44:46 +0200

    Remove user stuff and run `hello-world` docker image once install is done

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -2,13 +2,7 @@
 
 set -e
 
-INIT_USER="$(whoami)"
-export INIT_USER
-
 # Install docker
-sudo sh -c <<-'EOF'
-set -e
-
 apt-get -y update
 apt-get install -y ca-certificates curl
 
@@ -19,14 +13,13 @@ chmod a+r /etc/apt/keyrings/docker.asc
 os_arch="$(dpkg --print-architecture)"
 os_release="$(. /etc/os-release && echo "$VERSION_CODENAME")"
 
-printf 'deb [arch=%s signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian %s stable' -- "${os_arch}" "${os_release}" | \
+printf -- 'deb [arch=%s signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian %s stable' "${os_arch}" "${os_release}" | \
     tee /etc/apt/sources.list.d/docker.list > /dev/null
 
 apt-get -y update
 apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
-usermod -aG docker "${INIT_USER}"
-EOF
+docker run hello-world > /dev/null
 
 # TODO install k3s
 # TODO install k3d
