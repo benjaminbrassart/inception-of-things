4bc548b1ee98cef83dca001ab5040f66cb184fd0
Benjamin Brassart <bbrassar@student.42.fr>
2025-05-15 14:55:14 +0200

    Remove k3s installation

--- p3/scripts/init.sh
+++ p3/scripts/init.sh
@@ -6,10 +6,6 @@ tmp="$(mktemp -d)"
 
 trap 'rm -rf "${tmp}"' EXIT
 
-k3s_install_url="https://raw.githubusercontent.com/k3s-io/k3s/079ffa8d99fb859cb8c001455e47efa65535d832/install.sh"
-k3s_install_sha256="d75e014f2d2ab5d30a318efa5c326f3b0b7596f194afcff90fa7a7a91166d5f7"
-k3s_install="${tmp}/k3s_install.sh"
-
 k3d_install_url="https://raw.githubusercontent.com/k3d-io/k3d/201b614ea6798f0ee9e75c63360eef891b36bb19/install.sh"
 k3d_install_sha256="0f6bd9a3ab8ea8625843b21657a6538f117975bb2823b73200dc8b92ccc626de"
 k3d_install="${tmp}/k3d_install.sh"
@@ -41,8 +37,6 @@ usermod -aG docker bbrassar
 
 node_ip4="$(hostname -I | cut -d ' ' -f 1)"
 
-# Download k3s install script
-curl -fsSL "${k3s_install_url}" -o "${k3s_install}"
 # Download k3d install script
 curl -fsSL "${k3d_install_url}" -o "${k3d_install}"
 # Download argocd cli
@@ -50,19 +44,11 @@ curl -fsSL "${argocd_cli_url}" -o "${argocd_cli}"
 
 # Verify checksums
 {
-    printf -- '%s  %s\n' "${k3s_install_sha256}" "${k3s_install}"
     printf -- '%s  %s\n' "${k3d_install_sha256}" "${k3d_install}"
     printf -- '%s  %s\n' "${argocd_cli_sha256}" "${argocd_cli}"
 } | sha256sum -c -
 
-chmod +x "${k3s_install}" "${k3d_install}" "${argocd_cli}"
-
-# Install k3s
-"${k3s_install}" \
-    --cluster-init \
-    --write-kubeconfig-mode=644 \
-    --node-ip="${node_ip4}" \
-    --bind-address="${node_ip4}"
+chmod +x "${k3d_install}" "${argocd_cli}"
 
 # Install k3d
 "${k3d_install}"
