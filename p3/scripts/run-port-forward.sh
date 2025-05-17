08bdb04a56786b84d895cb4e1d817f9a7d1f14ad
Benjamin Brassart <bbrassar@student.42.fr>
2025-05-17 15:46:00 +0200

    Make port-forward for Argo CD only

--- p3/scripts/run-port-forward.sh
+++ p3/scripts/run-port-forward.sh
@@ -1,6 +1,3 @@
 #!/usr/bin/env sh
 
-kubectl port-forward svc/argocd-server -n argocd --address 0.0.0.0 8080:443 &
-kubectl port-forward svc/playground -n dev --address 0.0.0.0 8888:8888 &
-
-wait
+kubectl port-forward svc/argocd-server --namespace argocd --address 0.0.0.0 8080:443
