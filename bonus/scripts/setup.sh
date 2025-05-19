08671bc75a1bb53ef62f0b16a00b6e81ca5efc0c
Benjamin Brassart <bbrassar@student.42.fr>
2025-05-19 20:16:42 +0200

    Remove port forward for now

--- bonus/scripts/setup.sh
+++ bonus/scripts/setup.sh
@@ -3,5 +3,4 @@
 set -e
 
 k3d cluster create bonus \
-    --kubeconfig-update-default --kubeconfig-switch-context \
-    --port 18888:30080@server
+    --kubeconfig-update-default --kubeconfig-switch-context
