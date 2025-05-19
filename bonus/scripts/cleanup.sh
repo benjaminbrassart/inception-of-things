80162fa07fa9229c866a4e8f4bb0563df4fad3b1
Benjamin Brassart <bbrassar@student.42.fr>
2025-05-19 20:44:48 +0200

    Install gitlab in its own namespace

--- bonus/scripts/cleanup.sh
+++ bonus/scripts/cleanup.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env sh
 
+helm uninstall gitlab --namespace gitlab
 helm repo remove gitlab
--- bonus/scripts/setup.sh
+++ bonus/scripts/setup.sh
@@ -4,3 +4,7 @@ set -e
 set -x
 
 helm repo add gitlab https://charts.gitlab.io/
+helm install gitlab --create-namespace --namespace gitlab gitlab/gitlab \
+    --set installCertmanager=false \
+    --set global.ingress.configureCertmanager=false \
+    --set gitlab-runner.install=false
